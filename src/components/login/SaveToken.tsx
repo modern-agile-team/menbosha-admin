79fa7efa7be8516ee102b08d466fd645d19ab4ff
zzzRYT <jinjinstar3@gmail.com>
2024-04-26 16:31:45 +0900

    setting(#9):vercel설정2

--- src/components/login/SaveToken.tsx
+++ src/components/login/SaveToken.tsx
@@ -1,16 +1,11 @@
-"use client";
-
-import { useEffect } from "react";
 import AUTH from "@/apis/auth";
 import { useAuthStore } from "@/store/authStore";
-import { useRouter } from "next/navigation";
 
 interface Company {
   provider: string;
 }
 
 const SaveToken = ({ provider }: Company) => {
-  const router = useRouter();
   const { setLogin } = useAuthStore();
 
   const getToken = async () => {
@@ -23,13 +18,8 @@ const SaveToken = ({ provider }: Company) => {
       setLogin();
     } catch (err) {
       alert("로그인 도중 오류가 발생했습니다");
-      console.log(err);
     }
   };
-
-  useEffect(() => {
-    getToken();
-  }, []);
 
   return (
     <div>
