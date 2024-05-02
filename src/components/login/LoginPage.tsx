b55f131047195d0c859d89bf856b99f1b4b2a008
zzzRYT <jinjinstar3@gmail.com>
2024-05-02 18:18:27 +0900

    setting(#14):라우트 추가

--- src/components/login/LoginPage.tsx
+++ src/components/login/LoginPage.tsx
@@ -1,14 +1,19 @@
 /** @jsxImportSource @emotion/react */
 
 import * as S from "@/styles/login-styles/LoginStyled";
+import Link from "next/link";
 
 const LoginPage = () => {
   return (
     <div css={S.LoginContainer}>
       <div css={S.LoginBox}>
         <div>로그인</div>
-        <div css={S.AuthProviderImg}>네이버로그인</div>
-        <div css={S.AuthProviderImg}>카카오로그인</div>
+        <Link href="/report" css={S.AuthProviderImg}>
+          <div>네이버로그인</div>
+        </Link>
+        <Link href="/ban" css={S.AuthProviderImg}>
+          <div>카카오로그인</div>
+        </Link>
         <div css={S.AuthProviderImg}>구글로그인</div>
       </div>
     </div>
